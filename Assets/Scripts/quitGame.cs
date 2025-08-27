6134318f0eabeeef1775b8091d16f006e6bc2589
King Kiaku <b00160936@mytudublin.ie>
2025-08-28 00:55:26 +0100

    fixed small bug + sourcex

--- Assets/Scripts/quitGame.cs
+++ Assets/Scripts/quitGame.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class quitGame : MonoBehaviour
 {
+    public pauseMenu pauseMenu;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,7 +17,10 @@ public class quitGame : MonoBehaviour
 
     public void quitApplication()
     {
-        
+
         Application.Quit();
+        //This is here so if the game is paused and quit it will unpause  
+        Time.timeScale = 1f;
+        pauseMenu.GameIsPaused = false;
     }
 }
