04841443092d0c9e2a27e08436ccf662302f4b9e
King Kiaku <b00160936@mytudublin.ie>
2025-08-29 20:49:51 +0100

    Fixed a bug, new build, video + sources fixed
    Added the final WebGL build output and supporting files under 'Final Build', including data, framework, loader, and template assets. Updated sceneSwitch.cs to reset time scale and pause state when changing scenes. Updated project settings and documentation, and removed obsolete build artifacts.

--- Assets/Scripts/sceneSwitch.cs
+++ Assets/Scripts/sceneSwitch.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class sceneSwitch : MonoBehaviour
 {
     [SerializeField] public string sceneName; // Set the name of the scene you want to change to in the inspector
+    public pauseMenu pauseMenu;
 
     void Start()
     {
@@ -13,8 +14,11 @@ public class sceneSwitch : MonoBehaviour
 
     public void changeScene()
     {
-        //When button is pressed it will load sceneName 
+        //When button is pressed it will load sceneName
+         
         SceneManager.LoadScene(sceneName);
+         Time.timeScale = 1f;
+        pauseMenu.GameIsPaused = false;
     }
     
 
